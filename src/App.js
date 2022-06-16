dc64bf683540c27ac2710d68fd65acd7cc1426b1
Sid Ahmed Sahraoui <s.sahraoui@esi-sba.dz>
2022-06-17 00:45:09 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
 import Button from '@mui/material/Button';
-
+import Login from "./components/auth/login";
 function App() {
   return (
     <div className="App">
         <Button variant="contained">Hello World</Button>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={Login} />
+          </Routes>
+        </BrowserRouter>
 
     </div>
   );
---
+++ src/components/auth/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const Login = () => {
+
+    return(
+        <>
+        <Helmet>
+            <title>My App | login</title>
+        </Helmet>
+        <h4>hello</h4>
+        </>
+    )
+}
+
+export default Login
